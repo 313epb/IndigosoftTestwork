d70ba7618665161c04c5b1c764dc02e2759584ba
DMITRII DERYUGIN <44314455+313epb@users.noreply.github.com>
2026-05-31 19:53:43 +0400

    [IST-1]: Клиенты

---
+++ App/Services/ClientZ.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Channels;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace App.Services;
+
+public class ClientZ : IExchangeClient
+{
+    private readonly ILogger<ClientZ> _logger;
+
+    public async Task StartAsync(ChannelWriter<Tick> writer, CancellationToken ct)
+    {
+        while (!ct.IsCancellationRequested)
+        {
+            try
+            {
+                using (var socket= new ClientWebSocket())
+                {
+                    await socket.ConnectAsync(new Uri("ws://localhost:8080"), ct);
+                    _logger.LogInformation($"{nameof(ClientZ)} connected");
+                    var buffer= new byte[2048];
+                    while (socket.State == WebSocketState.Open)
+                    {
+                        var result = await socket.ReceiveAsync(buffer, ct);
+
+                        var json = Encoding.UTF8.GetString(
+                            buffer,
+                            0,
+                            result.Count);
+
+                        var tick = Parse(json);
+                        await writer.WriteAsync(tick, ct);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                await Task.Delay(1000, ct);
+            }
+        }
+    }
+    
+    private Tick Parse(string json)
+    {
+        //Тут в зависимости от формата  входной строки парсим в тик. 
+        return new Tick
+        {
+            Timestamp =  DateTime.Now
+        };
+    }
+}
--- App/obj/Debug/net9.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net9.0/App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae7c1ebdff5117a78772a664115f69319d92d5ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa83f3547fd778c5dddc9f62028e7b2373032dd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
